397e1f092dd64cf45933db337e4e2256987b19df
Sumit Sagar <sumitsagar2612@gmail.com>
2025-03-17 21:56:21 +0530

    added floating chatbot

---
+++ src/components/floatingChatbot.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { FaRobot } from "react-icons/fa";
+
+const FloatingChatbot = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [prevPath, setPrevPath] = useState(null);
+
+  
+  const excludedRoutes = ["/home", "/login", "/signup", "/quiz"];
+
+  if (excludedRoutes.includes(location.pathname)) {
+    return null;
+  }
+
+  const handleChatbotClick = () => {
+    if (location.pathname === "/chat") {
+      navigate(prevPath || "/");
+    } else {
+      setPrevPath(location.pathname);
+      navigate("/chat");
+    }
+  };
+
+  return (
+    <div className="fixed bottom-12 right-12 z-50">
+      <button
+        className="w-16 h-16 opacity-50 bg-green-700 text-white rounded-full flex items-center justify-center shadow-2xl hover:bg-pink-600 transition"
+        onClick={handleChatbotClick}
+      >
+        <FaRobot size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default FloatingChatbot;
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,6 +2,7 @@
 import Footer from '../components/Footer'
 import { Outlet,useLocation } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
+import FloatingChatbot from '../components/floatingChatbot';
 
 function Layout() {
   const location = useLocation();
@@ -10,7 +11,8 @@ function Layout() {
       {/* <Navbar /> */}
       <Sidebar />
       {<Outlet />}
-      {location.pathname !== "/profile" || location.pathname !== "/courses" && <Footer />}
+      {location.pathname !== "/profile" && location.pathname !== "/courses" && <Footer />}
+      <FloatingChatbot />
     </div>
   )
 }
